9b78dafa64ca4603c09ee18e57db17279a423153
Youcef Lemsafer <3415668+youcefl@users.noreply.github.com>
2025-10-01 18:26:14 +0200

    Initial import, day 3

---
+++ sc-client/src/lib.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+
+pub fn encrypt_file(file_to_encrypt: File) {
+    
+}
+
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2+2;
+        assert_eq!(result, 4);
+    }
+}
