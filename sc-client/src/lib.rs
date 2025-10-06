b7a6f557a2b068a5f54e4967bb8e9ec55423543c
Youcef Lemsafer <3415668+youcefl@users.noreply.github.com>
2025-10-06 20:48:28 +0200

    Metadata versioning + allow different kind of files. Temporarily increased uploaded file size limit to 60MB. Update regarding website folder structure. Remove = character padding at the end of file ids.

--- sc-client/src/lib.rs
+++ sc-client/src/lib.rs
@@ -4,8 +4,10 @@
 * Copyright PrimeScape
 */
 
+use std::io::Cursor;
+use byteorder::{ReadBytesExt, LittleEndian};
 use rand::RngCore;
-use sc_shared::{FileHeader, Metadata};
+use sc_shared::{FileHeader, Metadata, FileType};
 use web_sys::window;
 use anyhow::Result;
 use aes_gcm::{
@@ -99,13 +101,18 @@ fn do_encrypt_file(file_name: &str, data: &[u8], password: &str, file_header: &m
 
     let (padded_data, padding_size) = add_padding(data);
     let meta_data = Metadata{
+        file_type: FileType::RegularFile,
         file_name: String::from(file_name),
         file_size: data.len() as u64,
         padding_size: padding_size
     };
-    // Encrypt {serialized metadata}{padded_data}
-    let mut to_encrypt = meta_data.to_bytes()
+    // Encrypt {metadata version:2 bytes LE}{serialized metadata length:4 bytes LE}
+    //         {serialized metadata}{padded_data}
+    let mut to_encrypt = (meta_data.version() as u16).to_le_bytes().to_vec();
+    let metadata_bytes = meta_data.to_bytes()
             .map_err(|e| JsValue::from_str(&e.to_string()))?;
+    to_encrypt.extend((metadata_bytes.len() as u32).to_le_bytes());
+    to_encrypt.extend(metadata_bytes);
     to_encrypt.extend(padded_data);
     let cipher = Aes256Gcm::new(&key.into());
     let ciphertext = cipher.encrypt(nonce, to_encrypt.as_slice())
@@ -152,11 +159,28 @@ pub fn decrypt_file(data: &[u8], password: &str)
     let nonce = Nonce::from_slice(&file_header.init_vector);
     let decrypted_data = cipher.decrypt(nonce, remaining_data)
             .map_err(|e| JsValue::from_str(&e.to_string()))?;
-    // decrypted_data is expected to start with metadata
-    let (metadata, metadata_size) = Metadata::from_bytes_ex(&decrypted_data)
+    // decrypted_data is expected to look like this:
+    // metadata version (2 bytes unsigned LE)
+    // metadata size (4 bytes unsigned LE)
+    // metadata
+    // data
+    // padding
+    let map_err_js = |e| JsValue::from_str(&format!("IO error: {}", e));    
+    let mut cursor = Cursor::new(&decrypted_data);
+    let metadata_version = cursor.read_u16::<LittleEndian>().map_err(map_err_js)?;
+    let metadata_length = cursor.read_u32::<LittleEndian>().map_err(map_err_js)?;
+    let metadata_heading_size = size_of_val(&metadata_version) + size_of_val(&metadata_length);
+    let (metadata, metadata_size) = Metadata::from_bytes_ex(
+        &decrypted_data[metadata_heading_size..])
             .map_err(|e| JsValue::from_str(&e.to_string()))?;
-    // {metadata}{data}{padding}
-    let file_data = decrypted_data[metadata_size..metadata_size+metadata.file_size as usize].to_vec();
+    // Check that read bytes == expected metadata length
+    if metadata_length as usize != metadata_size {
+        return Err(JsValue::from_str("Invalid/corrupt file (bad metadata)"));
+    }
+    // {metadata version}{metadata length}{metadata}{data}{padding}
+    let file_data = decrypted_data[
+        metadata_heading_size + metadata_size..
+        metadata_heading_size + metadata_size + metadata.file_size as usize].to_vec();
     Ok(DecryptionResult::new(metadata.file_name, file_data))
 }
 
